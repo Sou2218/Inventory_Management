c6cc34b6d6a86904c5b294e4da69e200231d08b5
Your Name <youremail@example.com>
2025-07-27 18:01:20 +0530

    modified

--- backend_Inventory/server.js
+++ backend_Inventory/server.js
@@ -10,7 +10,7 @@ const app = express()
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://inventory-management-do53.vercel.app/'
+  'https://inventory-management-do53.vercel.app'
 ];
 
 const corsOptions = {
