fbb96007aba960091caed002929bbff85fd152cc
Your Name <youremail@example.com>
2025-07-27 18:32:07 +0530

    modified

--- backend_Inventory/server.js
+++ backend_Inventory/server.js
@@ -8,41 +8,17 @@ dotenv.config();
 const app = express();
 
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://inventory-management-flame-gamma.vercel.app",
-  "https://inventory-management-a76ghkzqz-sou2218s-projects.vercel.app",
-  "https://inventory-management-do53.vercel.app",
-  "https://inventory-management-do53-git-main-sou2218s-projects.vercel.app"
-];
+app.use(cors());
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      console.error("❌ CORS blocked request from:", origin);
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true, 
-  optionsSuccessStatus: 200 
-};
-
-app.use(cors(corsOptions));
 app.use(express.json());
-
-// Routes
 app.use('/api/products', productRoutes);
 
-// Health Check
 app.get('/', (req, res) => {
-  res.send('Backend is running');
+  res.send(' Backend is running');
 });
 
-// Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   connectDB();
-  console.log(`Server started @ http://localhost:${PORT}`);
+  console.log(` Server started at http://localhost:${PORT}`);
 });
