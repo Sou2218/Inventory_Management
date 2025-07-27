41c5d5fc12bc686bc36de0543b85dbdffe71b979
Your Name <youremail@example.com>
2025-07-27 19:51:53 +0530

    modified

--- backend_Inventory/server.js
+++ backend_Inventory/server.js
@@ -8,25 +8,23 @@ dotenv.config();
 const app = express();
 
 const corsConfig = {
-  origin: "*",
+  origin: ["https://inventory-management-do53.vercel.app"], 
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"]
 };
 
-
 app.use(cors(corsConfig));
+app.options("*", cors(corsConfig)); // handle preflight requests
 
-app.options("*", cors(corsConfig));
 app.use(express.json());
 app.use('/api/products', productRoutes);
 
 app.get('/', (req, res) => {
-  res.send(' Backend is running');
+  res.send('Backend is running');
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   connectDB();
-
-  console.log(` Server started at http://localhost:${PORT}`);
+  console.log(`Server started at http://localhost:${PORT}`);
 });
